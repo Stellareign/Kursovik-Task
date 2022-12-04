05e698af72b32eccf55e48173d0224d4b4682888
Stellareign <tuguran@mail.ru>
2022-12-04 18:00:46 +0300

    Задача. Чуток с доработкой.

---
+++ src/Calc.java
@@ -0,0 +1,9 @@
+public class Calc {
+    public static double quadrate(double x) {
+        double j = 2;
+        double quadrateNumber = Math.pow(x, j);
+        System.out.println("Возведение числа х в степень: "  + j + " равно " + quadrateNumber);
+        return quadrateNumber;
+    }
+}
+
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        double x = 7;
+        Calc.quadrate(x);
+    }
+}
